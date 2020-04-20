2245436b6b91081d4ab99f52ff4a922ef67913c9
SevenFive <sevenfivepe@gmail.com>
2020-04-20 22:42:51 +0200

    add file pe 5 build in functions

---
+++ AutoPY5functions.py
@@ -0,0 +1,20 @@
+#Build-In Functions
+
+#print()
+#input()
+#len()
+
+#modules has to be inported first
+
+import random, sys, os, math
+random.randint(1, 10)
+
+
+#oprionally not to print "random." each time infront function (but not recommended
+import random
+from random import *
+randint(1, 10)
+
+#to terminate running program
+import sys
+sys.exit()
