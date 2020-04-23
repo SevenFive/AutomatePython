d9304b23a0dd0d458a72745b217d34327815df8e
SevenFive <sevenfivepe@gmail.com>
2020-04-23 18:24:04 +0200

    add py file Methods

---
+++ AutoPY14Methods.py
@@ -0,0 +1,12 @@
+# Methods are functions that are 'called on' values.
+
+# For instance methods for list are the following:
+#   index() returns the index of an item in the list
+#   append() adds a value to the end of a list
+#   insert() adds a value anywhere inside a list
+#   remove() removes an item, specified by the value, from a list
+#   sort() sorts the items in a list
+#   sort(reverse=True) keyword argument can sort in reverse order
+# Sorting is in ASCII-betical order to bypass it sort(key=str.lower)
+
+# code will be added later
