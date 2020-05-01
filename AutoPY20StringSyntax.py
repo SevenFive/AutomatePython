02b0fb3c8d052b0deeedd60b0bf578c23de17864
SevenFive <sevenfivepe@gmail.com>
2020-05-01 13:28:52 +0200

    add PY file string syntax

---
+++ AutoPY20StringSyntax.py
@@ -0,0 +1,23 @@
+# String Syntax advanced
+# \'    single quote
+# \"    double quote
+# \t    tab
+# \n    newline (line break)
+# \\    backslash
+
+# to start a new line in string use "\n"
+hello = 'Hello there my friends! \nHow are you? \nStay home and stay safe huh?'
+print(hello)
+print("Double 'quotes'. That\'s great.")
+
+# raw string - everything will be displayed
+print(r'That is a \'raw\' string.')
+
+# multi line string """ """
+print("""Dear Mr. Smith,
+
+Hope you  are well.
+Please check your email for more information.
+
+Sincerely,
+Neo""")
