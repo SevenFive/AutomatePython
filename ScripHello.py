0c7c7c573334766d8198e51e742811ef02ef5b88
SevenFive <sevenfivepe@gmail.com>
2020-05-03 00:08:44 +0200

    add script hello

---
+++ ScripHello.py
@@ -0,0 +1,8 @@
+#! python3
+
+#   all python programs for OSs should start with Shebang line
+#   Windows (version check but not required in general): #! python3
+#   OS X: #! /usr/bin/env python3
+#   Linux: #! /usr/bin/python3
+
+print('Hello there!')
